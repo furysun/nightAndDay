539628b36498b5faef523e73eba0505671e9ffdd
furysun <fury.sun.main@gmail.com>
2021-04-18 20:37:46 +0300

    +

--- index.js
+++ index.js
@@ -5,18 +5,16 @@ let isBlack = false;
 const themes = [
     {
         name: 'black',
-        headerBackgroundImage: '../img/m-h-night.jpg',
+        headerBackgroundImage: './img/m-h-night.jpg',
         backgroundColor: 'rgb(33,35,46)',
         headerHeadingColor: 'rgb(255,255,255)',
         highlightedTextColor: 'rgb(252, 79, 60)',
         headerTextColor: 'rgb(255,255,255)',
         logoLogoColor: 'rgb(255,255,255)',
-        buttonColor:'rgb(233, 173, 39)',
-        sectionTextColor:'rgb(255,255,255)',
-        sectionCardColor:'rgb(59,63,79)',
-
-        // gradientColorOne:'darkseagreen',
-        gradientColorOne:'linear-gradient(to right, rgb(220, 162, 39),  rgb(150, 55, 77))'
+        buttonColor: 'rgb(233, 173, 39)',
+        sectionTextColor: 'rgb(255,255,255)',
+        sectionCardColor: 'rgb(59,63,79)',
+        gradient: 'linear-gradient(to right, rgb(220, 162, 39),  rgb(150, 55, 77))'
     },
     {
         name: 'white',
@@ -26,12 +24,10 @@ const themes = [
         highlightedTextColor: 'rgb(35, 160, 197)',
         headerTextColor: 'rgb(32, 34, 45)',
         logoLogoColor: 'rgb(32, 34, 45)',
-        buttonColor:'rgb(106, 152, 26)',
-        sectionTextColor:'rgb(32, 34, 45)',
-        sectionCardColor:'rgb(32, 34, 45)',
-        gradientColorOne:'linear-gradient(to right, rgb(98, 143, 40),  rgb(50, 95, 211))'
-
-
+        buttonColor: 'rgb(106, 152, 26)',
+        sectionTextColor: 'rgb(32, 34, 45)',
+        sectionCardColor: 'rgb(32, 34, 45)',
+        gradient: 'linear-gradient(to right, rgb(98, 143, 40),  rgb(50, 95, 211))'
     }
 ];
 
@@ -55,33 +51,32 @@ function switchTheme() {
     headerStyle.borderWidth = '0px 0px 90px 0px';
     headerStyle.borderStyle = 'solid';
 
-
     document.getElementsByTagName('body')[0].style.backgroundColor = currentTheme.backgroundColor;
     document.getElementsByClassName('header_heading')[0].style.color = currentTheme.headerHeadingColor;
 
-    document.getElementsByClassName('highlighted-text')[0].style.backgroundColor = currentTheme.highlightedTextColor;
-    document.getElementsByClassName('highlighted-text')[1].style.backgroundColor = currentTheme.highlightedTextColor;
-    document.getElementsByClassName('highlighted-text')[2].style.backgroundColor = currentTheme.highlightedTextColor;
+    let highlightedTexts = document.getElementsByClassName('highlighted-text');
+    highlightedTexts[0].style.backgroundColor = currentTheme.highlightedTextColor;
+    highlightedTexts[1].style.backgroundColor = currentTheme.highlightedTextColor;
+    highlightedTexts[2].style.backgroundColor = currentTheme.highlightedTextColor;
 
     document.getElementsByClassName('header_text')[0].style.color = currentTheme.headerTextColor;
     document.getElementsByClassName('section_banner_heading')[0].style.color = currentTheme.headerTextColor;
     document.getElementsByClassName('logo-logo')[0].style.color = currentTheme.logoLogoColor;
 
-    document.getElementsByClassName('header_button')[0].style.backgroundColor = currentTheme.buttonColor;
-    document.getElementsByClassName('header_button')[1].style.backgroundColor = currentTheme.buttonColor;
+    let headerButtons = document.getElementsByClassName('header_button');
+    headerButtons[0].style.backgroundColor = currentTheme.buttonColor;
+    headerButtons[1].style.backgroundColor = currentTheme.buttonColor;
     document.getElementsByClassName('section_banner_button')[0].style.backgroundColor = currentTheme.buttonColor;
 
     document.getElementsByClassName('section_text')[0].style.color = currentTheme.sectionTextColor;
 
-    document.getElementsByClassName('section_card')[0].style.backgroundColor = currentTheme.sectionCardColor;
-    document.getElementsByClassName('section_card')[1].style.backgroundColor = currentTheme.sectionCardColor;
-    document.getElementsByClassName('section_card')[2].style.backgroundColor = currentTheme.sectionCardColor;
+    let cards = document.getElementsByClassName('section_card');
+    cards[0].style.backgroundColor = currentTheme.sectionCardColor;
+    cards[1].style.backgroundColor = currentTheme.sectionCardColor;
+    cards[2].style.backgroundColor = currentTheme.sectionCardColor;
     document.getElementsByClassName('footer')[0].style.backgroundColor = currentTheme.sectionCardColor;
 
-    document.getElementsByClassName('section_banner')[0].style.backgroundImage = currentTheme.gradientColorOne;
-
-    // ....
-
+    document.getElementsByClassName('section_banner')[0].style.backgroundImage = currentTheme.gradient;
 }
 
 document.addEventListener("DOMContentLoaded", onDomLoaded);
